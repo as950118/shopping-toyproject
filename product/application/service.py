46e94670a6233769d32e90cff78090d5a0626969
jeongheonjin <as950118@gmail.com>
2025-04-30 16:40:24 +0900

    [Feat] app 1. 상품관련 유즈케이스

---
+++ product/application/service.py
@@ -0,0 +1,24 @@
+from typing import List, Optional
+from product.domain.models import Product
+from product.application.port import ProductUseCase, ProductRepository
+
+class ProductService(ProductUseCase):
+    """
+    상품 관련 유스케이스(비즈니스 로직) 구현체.
+    ProductRepository(출력 포트)에 의존하여 데이터 접근.
+    """
+
+    def __init__(self, repository: ProductRepository):
+        self.repository = repository
+
+    def list_products(self) -> List[Product]:
+        return self.repository.get_all()
+
+    def get_product_detail(self, product_id: int) -> Optional[Product]:
+        return self.repository.get_by_id(product_id)
+
+    def calculate_final_price(self, product_id: int) -> Optional[int]:
+        product = self.repository.get_by_id(product_id)
+        if product is None:
+            return None
+        return product.calculate_final_price()
